591575e8ada5b5ca0ff681263da1267c3c7c4e33
roshray <roshanroy759@gmail.com>
2022-03-02 00:36:24 +0530

    all

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200'>
+        <h1>2022 vector Search Engine, Inc.</h1>
+    </div>
+  )
+}
+
